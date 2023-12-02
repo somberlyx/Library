38e3cff38050ac9dfb0725f35e47ae6e7a2b8aa4
somberlyx <lybasiddiqui02@gmail.com>
2023-12-02 20:09:09 +0400

    Create addToLibraryFunction()

--- library.js
+++ library.js
@@ -13,5 +13,25 @@ closeBtn.addEventListener("click", () => {
   dialog.close();
 });
 
+confirmBtn.addEventListener("click", (event) => {
+  addBookToLibrary();
+  addCard();
+  event.preventDefault();
+  dialog.close();
+})
+
 function Book() {
+}
+
+function addBookToLibrary(){
+  const bookTitle = document.querySelector("dialog .title");
+  const author = document.querySelector("dialog .writtenBy");
+  const date = document.querySelector("dialog .published");
+  const pages = document.querySelector("dialog .pages");
+  const description = document.querySelector("dialog .description")
+  const read = document.querySelector("dialog input[name='Reading']:checked");
+  let book = new Book(bookTitle.value, author.value, date.value, pages.value, description.value, read.value)
+  console.log(book)
+  library.push(book);
+  console.log(library);
 }
